7f5aa57b4ed1bd0a2989295798bb3aa7a2e74866
Izaak Prats <izaak.prats@gmail.com>
2017-06-06 16:39:18 -0700

    init with a uiimagepickercontroller

--- MLCoreTesting/ViewController.swift
+++ MLCoreTesting/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func selectPhotoButtonTapped(_ sender: UIButton) {
+        // Prompt user
+        self.present(imagePicker, animated: true)
+    }
+    
+    private var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupImagePicker()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupImagePicker() {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.mediaTypes = [kUTTypeJPEG as String]
+        imagePicker.sourceType = .photoLibrary
+    }
 }
 
+extension ViewController: UINavigationControllerDelegate {
+    // TODO
+    // Don't think I actually need to do anything here.
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        defer { imagePicker.dismiss(animated: true) }
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+    }
+}
